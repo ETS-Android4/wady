20d3cd6d272a791846e401362abc71f60d570ae5
murpave <pavel.muravyov@gmail.com>
2014-05-21 18:08:52 +0300

    Minor cleanup

--- core/src/com/mpv/data/Assets.java
+++ core/src/com/mpv/data/Assets.java
@@ -127,13 +127,4 @@ public class Assets {
 		if (Settings.soundEnabled) 	sound.play(volume, pitch, 0);
 	}
 	
-	public static void fixBleeding(TextureRegion region) {
-		float x = region.getRegionX();
-		float y = region.getRegionY();
-		float width = region.getRegionWidth();
-		float height = region.getRegionHeight();
-		float invTexWidth = 1f / region.getTexture().getWidth();
-		float invTexHeight = 1f / region.getTexture().getHeight();
-		region.setRegion((x + .5f) * invTexWidth, (y+.5f) * invTexHeight, (x + width - .5f) * invTexWidth, (y + height - .5f) * invTexHeight);       
-	}
 }
