5f9398930b57dd5f880575f35faae794d13ea5a3
pmuravyov <pavel.muravyov@gmail.com>
2015-01-11 19:15:32 +0300

    Skin updates, developing new level screeen

--- core/src/com/mpv/screens/stages/MainMenuStage.java
+++ core/src/com/mpv/screens/stages/MainMenuStage.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mpv.data.Assets;
 import com.mpv.data.Const;
@@ -20,96 +22,86 @@ public class MainMenuStage extends Stage {
 
 	public MainMenuStage() {
 		super();
-		float width=this.getWidth()/3.2f;
-		float height=Const.PLAYER_SIZE*GVars.BOX_TO_WORLD/1.63f;
-		float yPos1=this.getHeight() - this.getHeight()*0.25f;
-		float yPos2=yPos1 + this.getHeight()*0.125f;
+		float width = this.getWidth() / 1.6f;
+		float height = Const.PLAYER_SIZE * GVars.BOX_TO_WORLD / 1.6f;
 
 		Image image = new Image(Assets.skin.getDrawable("menu-screen"));
-		image.setSize(getWidth(), getWidth()/image.getWidth()*image.getHeight());
+		image.setSize(getWidth(), getWidth() / image.getWidth() * image.getHeight());
 		image.setPosition(0, 0);
 		this.addActor(image);
 
 		exitDialog = new ExitDialog("", Assets.skin, "default");
-		
+
 		final Button bMusic = new Button(Assets.skin, "music-button");
-		final Button bSound  = new Button(Assets.skin, "sound-button");
-		
-		final TextButton bNewGame = new TextButton("Play", Assets.skin, "menu-button");
-		//final TextButton bHighScores = new TextButton("Scores", Assets.skin, "menu-button");
-		final TextButton bCredits = new TextButton("Credits", Assets.skin, "menu-button");
-		final TextButton bExit = new TextButton("Exit", Assets.skin, "menu-button");
-		
-		bMusic.setSize(width/3f, width/3f);
-		bMusic.setChecked(!Settings.musicEnabled);
-		
-		bSound.setSize(width/3f, width/3f);
-		bSound.setChecked(!Settings.soundEnabled);
-		
-		bNewGame.setSize(width, height);
-		//bHighScores.setSize(width, height);
-		bCredits.setSize(width, height);
-		bExit.setSize(width, height);
-		
-		bNewGame.setPosition(getWidth()*(-0.01f), yPos1);
-		//bHighScores.setPosition(getWidth()*0.22f, yPos);
-		bCredits.setPosition(getWidth()*0.35f, yPos1);
-		bExit.setPosition(getWidth()*0.75f, yPos1);
-		bSound.setPosition(getWidth()*0.51f, yPos2);
-		bMusic.setPosition(getWidth()*0.39f, yPos2);
-		
-		this.addActor(bNewGame);
-		//this.addActor(bHighScores);
-		this.addActor(bCredits);
-		this.addActor(bExit);
-		this.addActor(bSound);
-		this.addActor(bMusic);
-		
+		final Button bSound = new Button(Assets.skin, "sound-button");
+
+		final TextButton bNewGame = new TextButton("Play", Assets.skin);
+		// final TextButton bHighScores = new TextButton("Scores", Assets.skin, "menu-button");
+		final TextButton bCredits = new TextButton("Credits", Assets.skin);
+		final TextButton bExit = new TextButton("Exit", Assets.skin);
+
+		Table buttonTable = new Table();
+		Table sndTable = new Table();
+		Table parentTable = new Table();
+		parentTable.setFillParent(true);
+		sndTable.add(bSound).center().pad(height / 4f);
+		sndTable.add(bMusic).center().pad(height / 4f);
+
+		buttonTable.setBackground(Assets.skin.getDrawable("window"));
+		buttonTable.align(Align.center);
+		this.addActor(parentTable);
+		parentTable.add(buttonTable);
+
+		buttonTable.add(sndTable).size(height).pad(height / 4f).row();
+		buttonTable.add(bNewGame).size(width, height).pad(height / 4f).row();
+		buttonTable.add(bCredits).size(width, height).pad(height / 4f).row();
+		buttonTable.add(bExit).size(width, height).pad(height / 4f);
+
 		bMusic.addListener(new ClickListener() {
 			public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-				//super.touchDown(event, x, y, pointer, button);
+				// super.touchDown(event, x, y, pointer, button);
 				Settings.musicEnabled = !bMusic.isChecked();
 				Assets.playMusic(Assets.menuMusic);
 			}
 		});
 		bSound.addListener(new ClickListener() {
 			public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-				//super.touchDown(event, x, y, pointer, button);
+				// super.touchDown(event, x, y, pointer, button);
 				Settings.soundEnabled = !bSound.isChecked();
 			}
 		});
-		
+
 		// MENU buttons
 		bNewGame.addListener(new ClickListener() {
 			public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-				//super.touchDown(event, x, y, pointer, button);
+				// super.touchDown(event, x, y, pointer, button);
 				Assets.playSnd(Assets.buttonSnd);
-				GVars.app.setScreen(GVars.app.levelScreen);				
+				GVars.app.setScreen(GVars.app.levelScreen);
 			}
 		});
 
 		bCredits.addListener(new ClickListener() {
 			public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-				//super.touchDown(event, x, y, pointer, button);
+				// super.touchDown(event, x, y, pointer, button);
 				Assets.playSnd(Assets.buttonSnd);
 				GVars.app.setScreen(GVars.app.creditsScreen);
 			}
 		});
 		bExit.addListener(new ClickListener() {
 			public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-				//super.touchDown(event, x, y, pointer, button);
+				// super.touchDown(event, x, y, pointer, button);
 				Assets.playSnd(Assets.buttonSnd);
 				exitDialog.show(event.getStage());
 			}
-		});        
+		});
 		this.addListener(new InputListener() {
-			public boolean keyUp (InputEvent event, int keycode) {
+			public boolean keyUp(InputEvent event, int keycode) {
 				if (keycode == Keys.BACK || keycode == Keys.ESCAPE) {
 					exitDialog.show(event.getStage());
 				}
 				return false;
 			}
-		});     
+		});
 	}
 
 }
